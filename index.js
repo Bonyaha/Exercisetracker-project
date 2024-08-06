f7b47efde8d0baa0a009b21e1ada04e2ad31a92a
Roman <romchikus1987@gmail.com>
2024-08-06 17:56:08 +0300

    updated

--- index.js
+++ index.js
@@ -37,30 +37,95 @@ let User = mongoose.model('User', userSchema);
 const exerciseSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   description: String,
-  duratioin: Number,
+  duration: Number,
   date: { type: Date, default: Date.now },
 })
 // Create the model
 const Exercise = mongoose.model('Exercise', exerciseSchema);
 
-app.post('/api/users', async(req, res) => {
+app.post('/api/users', async (req, res) => {
   console.log('request is: ', req.body);
-  try{
-    const newUser = new User({username:req.body.username});
+  try {
+    const newUser = new User({ username: req.body.username });
     const savedUser = await newUser.save();
     res.json({ username: savedUser.username, _id: savedUser._id });
   }
-  catch(err){
+  catch (err) {
     res.status(500).send(err);
   }
 })
 
-app.get('/api/users', async(req,res)=>{
-  try{
+app.get('/api/users', async (req, res) => {
+  try {
     const users = await User.find({});
     console.log(users);
-    res.json(users.map(user=>({username:user.username, _id: user._id})))
-  }catch(err){
+    res.json(users.map(user => ({ username: user.username, _id: user._id })))
+  } catch (err) {
+    res.status(500).send(err);
+  }
+})
+
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  const { description, duration, date } = req.body
+  const userId = req.params._id;
+  const exerciseDate = date ? new Date(date) : new Date()
+  console.log('userId is: ', userId);
+  console.log('description is: ', description);
+  console.log('date is: ', exerciseDate);
+  console.log('duration is: ', typeof duration);
+  try {
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).send('User not found');
+    console.log('user is: ', user);
+    const newExercise = new Exercise({
+      userId: user._id,
+      description,
+      duration: parseInt(duration),
+      date: exerciseDate,
+    });
+    const savedExercise = await newExercise.save();
+    console.log('duration is: ', savedExercise.duration);
+    res.json({
+      _id: user._id,
+      username: user.username,
+      date: savedExercise.date.toDateString(), // Convert to dateString format,
+      duration: savedExercise.duration,
+      description: savedExercise.description
+    });
+  }
+  catch (err) {
+    res.status(500).send(err);
+  }
+})
+
+app.get('/api/users/:_id/logs', async (req, res) => {
+  const userId = req.params._id;
+  const { from, to, limit } = req.query;
+  
+  try {
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).send('User not found');
+    let filter = { userId }; // Initialize the filter with the userId
+    if (from || to) {
+      filter.date = {}; // Initialize the date filter
+
+      if (from) filter.date.$gte = new Date(from); // Add 'from' date to the filter
+      if (to) filter.date.$lte = new Date(to); // Add 'to' date to the filter
+    }
+    
+    const exercises = await Exercise.find(filter).limit(parseInt(limit));
+    console.log('exercises are: ', exercises);
+    res.json({
+      username: user.username,
+      count: exercises.length,
+      _id: user._id,
+      log: exercises.map(ex => ({
+        description: ex.description,
+        duration: ex.duration,
+        date: ex.date.toDateString(), // Convert to dateString format
+      })),
+    });
+  } catch (err) {
     res.status(500).send(err);
   }
 })
